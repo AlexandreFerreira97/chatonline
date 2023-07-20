fc90357da1b229b9a8d9bc4309d314763c835b2d
Alexandre <alexandrefaria3012@gmail.com>
2023-07-20 17:44:47 -0300

    Update chat_message.dart

--- lib/chat_message.dart
+++ lib/chat_message.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key? key, required this.data}) : super(key: key);
+
+  final Map<String, dynamic> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        children: [
+          CircleAvatar(backgroundImage: NetworkImage(data['senderPhotoUrl'])),
+          Expanded(
+              child: Column(
+            children: [
+              Text(
+                data['senderName'],
+                style: const TextStyle(
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
